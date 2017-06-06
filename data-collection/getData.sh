bd7d1a6ba2b6639d6e2a963b08fcadad8ce34010
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-06-06 16:34:55 -0400

    data-aggregation

---
+++ data-collection/getData.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read -p "If Hadoop isnt running on your system, would you like to start running it? [y|n]: " hdp
+if ["$hdp"="y"] || ["$hdp"="Y"]; then
+    docker run --hostname=quickstart.cloudera --privileged=true -t -i -p 8888:8888 -p 8000:8000 -p 8080:8080  cloudera/quickstart /usr/bin/docker-quickstart
+fi
+
+let containerID = docker ps | grep cloudera/quickstart | cut -d' ' -f1
+docker exec "$containerID" mkdir loadingZone
+docker exec "$containerID" wget http://bostonopendata-boston.opendata.arcgis.com/datasets/1d9509a8b2fd485d9ad471ba2fdb1f90_0.csv -O loadingZone/BuildBPS.csv
+docker exec "$containerID" wget http://bostonopendata-boston.opendata.arcgis.com/datasets/0046426a3e4340a6b025ad52b41be70a_1.csv -O loadingZone/Private.csv
+docker exec "$containerID" wget http://bostonopendata-boston.opendata.arcgis.com/datasets/1d9509a8b2fd485d9ad471ba2fdb1f90_0.csv -O loadingZone/Public.csv
+docker exec "$containerID" wget https://data.cityofboston.gov/api/views/yv8c-t43q/rows.csv?accessType=DOWNLOAD -O loadingZone/PropVal.csv
+
