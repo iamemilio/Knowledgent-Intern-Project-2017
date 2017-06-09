b8088c9b91644f38855499bf58af10f019bd5c42
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-06-09 11:37:35 -0400

    hive corrections --> hql script

--- data-collection/offline-datasets/createHiveTables.sh
+++ data-collection/offline-datasets/createHiveTables.sh
@@ -1,86 +1,86 @@
-mkdir ~/hive-raw-zone
-hadoop fs -mkdir data
-hadoop fs -mkdir data/hive
+mkdir hive-raw-zone
+touch hive-raw-zone/load-data.hql
 
 for file in $(ls ~/raw-zone)
 do
 let filename=$(cut -d$'.' -f1 $file)
 cat $file | cut -d$'\n' f2- > hive-raw-zone/$filename-stripped.csv
 
-#move files into hdfs
-hadoop fs -put ~/hive-raw-zone/ data/hive/"$file"
-
 #parse out the headers
 header=$($file | cut -d$'\n' -f1)
 IFS=',' read -r -a params <<< "$header"
 
-hive
 #create hive tables for data sets
 case $filename in
     "BuildBPS"*)
-        CREATE TABLE IF NONE EXISTS $filename ( {params[0]} int, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string) \
+        "CREATE TABLE IF NONE EXISTS \'$filename\' ( {params[0]} int, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string) \
         COMMENT ‘Location and Administrative info for Boston Public Schools’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hql
         ;;   
     
     "Earnings"*)
-        CREATE TABLE IF NONE EXISTS $filename ( ${params[0]} string, ${params[1]} string, ${params[2]} string, ${params[3]} string, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} int, ${params[11]} int, ${params[12]} int) \
+        "CREATE TABLE IF NONE EXISTS \'$filename\' ( ${params[0]} string, ${params[1]} string, ${params[2]} string, ${params[3]} string, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} int, ${params[11]} int, ${params[12]} int) \
         COMMENT ‘Salaries for Public Employees’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hql
         ;;    
 
     "EducatorEval"*)
-        CREATE TABLE IF NONE EXISTS $filename (  ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int) \
+        "CREATE TABLE IF NONE EXISTS \'$filename\' (  ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int) \
         COMMENT ‘Location and Administrative info for Boston Public Schools’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hql
         ;;
 
     "Gradsattendingcollege"*)
-        CREATE TABLE IF NONE EXISTS $filename ( ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} int, ${params[11]} int) \
+        "CREATE TABLE IF NONE EXISTS \'$filename\' ( ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} int, ${params[11]} int) \
         COMMENT ‘Location and Administrative info for Boston Public Schools’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hql
         ;;    
 
     "graduates"*)
-        CREATE TABLE IF NONE EXISTS $filename ( {params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} int) \
+       "CREATE TABLE IF NONE EXISTS \'$filename\' ( {params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} int, ${params[6]} int, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} int) \
         COMMENT ‘Location and Administrative info for Boston Public Schools’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hqls
         ;;
 
     "Private"*)
-        CREATE TABLE IF NONE EXISTS $filename ( {params[0]} int, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string) \
+        "CREATE TABLE IF NONE EXISTS \'$filename\' ( {params[0]} int, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string) \
         COMMENT ‘Location and Administrative info for Boston Public Schools’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hql
         ;;    
 
     "Public"*)
-        CREATE TABLE IF NONE EXISTS $filename ( {params[0]} int, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string) \
+        "CREATE TABLE IF NONE EXISTS \'$filename\' ( {params[0]} int, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string) \
         COMMENT ‘Location and Administrative info for Boston Public Schools’ \
         ROW FORMAT DELIMITED \
         FIELDS TERMINATED BY ‘,’ \
         LINES TERMINATED BY ‘\n’ \
-        STORED AS TEXTFILE;
+        STORED AS TEXTFILE;" >> hive-raw-zone/load-data.hql
         ;;    
 esac
-LOAD DATA INPATH '/data/hive/$file' INTO TABLE $filename
-exit;
-
+"LOAD DATA INPATH \'/data/hive/$file\' INTO TABLE $filename;" >> hive-raw-zone/load-data.hql
 done
+
+#move files into hdfs
+hadoop fs -mkdir data
+hadoop fs -mkdir data/hive
+hadoop fs -put ~/hive-raw-zone/ data/hive/
+
+#hive -f hive-raw-zone/load-data.hql
