6cae2155a7b8e8a9f0cfb2218ce7dcb9a41da9c6
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-06-09 13:37:41 -0400

    backtrack 1

--- data-collection/offline-datasets/createHiveTables.sh
+++ data-collection/offline-datasets/createHiveTables.sh
@@ -3,8 +3,8 @@ touch hive-raw-zone/load-data.hql
 
 for file in $(ls raw-zone/offline-datasets/)
 do
-filename=$(echo "$file" | cut -d$'.' -f1 )
-cat $file | cut -d$'\n' f2- > hive-raw-zone/$filename-stripped.csv
+filename=$(cut -d$'.' -f1 "$file")
+cat raw-zone/offline-datasets/$file | cut -d$'\n' f2- > hive-raw-zone/$filename-stripped.csv
 
 #parse out the headers
 header=$($file | cut -d$'\n' -f1)
