9bf957b931d0271a9690edada9776a4605907ce0
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-06-12 14:00:18 -0400

    lol pls make it stop

--- automated-data-aggregation/boston-school-data/test.sh
+++ automated-data-aggregation/boston-school-data/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-header=$(cut -d$'\n' -f 1 boston-school-data/raw-zone/buildbps.csv)
+header=$(cut -d$'\n' -f 1 raw-zone/buildbps.csv)
 IFS=',' read -r -a params <<< "$header"
 cat > idk.txt <<EOL
 CREATE TABLE IF NONE EXISTS '$filename' ( ${params[0]} string, ${params[1]} string, ${params[2]} numeric, ${params[3]} string, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} string, ${params[8]} numeric, ${params[9]} numeric, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} numeric, ${params[15]} numeric, ${params[16]} numeric, ${params[17]} numeric, ${params[18]} numeric, ${params[19]} numeric, ${params[20]} numeric,\
