b569f92e10670845c73a3c52cfd8e37a3582e031
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-06-14 12:07:43 -0400

    this hsould work now

--- automated-data-aggregation/boston-school-data/createHiveTables.sh
+++ automated-data-aggregation/boston-school-data/createHiveTables.sh
@@ -45,61 +45,60 @@ ${params[101]} string, ${params[102]} string, ${params[103]} string, ${params[10
 ${params[151]} string, ${params[152]} string, ${params[153]} string, ${params[154]} string, ${params[155]} string, ${params[156]} string, ${params[157]} string, ${params[158]} string, ${params[159]} string, ${params[160]} string, ${params[161]} string, ${params[162]} string, ${params[163]} string, ${params[164]} string, ${params[165]} string, ${params[166]} string, ${params[167]} string, ${params[168]} string, ${params[169]} string, ${params[170]} string, ${params[171]} string, ${params[172]} string, ${params[173]} string, ${params[174]} string, ${params[175]} string, ${params[176]} string, ${params[177]} string, ${params[178]} string, ${params[179]} string, ${params[180]} string, ${params[181]} string, ${params[182]} string, ${params[183]} string, ${params[184]} string, ${params[185]} string, ${params[186]} string, ${params[187]} string, ${params[188]} string, ${params[189]} string, ${params[190]} string, ${params[191]} string, ${params[192]} string, ${params[193]} string, ${params[194]} string, ${params[195]} string, ${params[196]} string, ${params[197]} string, ${params[198]} string, ${params[199]} string, ${params[200]} string, \
 ${params[201]} string, ${params[202]} string, ${params[203]} string, ${params[204]} string, ${params[205]} string, ${params[206]} string, ${params[207]} string, ${params[208]} string, ${params[209]} string, ${params[210]} string, ${params[211]} string, ${params[212]} string, ${params[213]} string, ${params[214]} string, ${params[215]} string, ${params[216]} string, ${params[217]} string, ${params[218]} string, ${params[219]} string, ${params[220]} string, ${params[221]} string, ${params[222]} string, ${params[223]} string, ${params[224]} string, ${params[225]} string, ${params[226]} string, ${params[227]} string, ${params[228]} string, ${params[229]} string, ${params[230]} string, ${params[231]} string, ${params[232]} string, ${params[233]} string, ${params[234]} string, ${params[235]} string, ${params[236]} string, ${params[237]} string, ${params[238]} string, ${params[239]} string, ${params[240]} string, ${params[241]} string, ${params[242]} string, ${params[243]} string, ${params[244]} string, ${params[245]} string, ${params[246]} string, ${params[247]} string, ${params[248]} string, ${params[249]} string, ${params[250]} string )  
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;
 
     "Employee_Earnings"*)
         echo "CREATE EXTERNAL TABLE IF NOT EXISTS boston_data.$filename ( ${params[0]} string, ${params[1]} string, ${params[2]} string, ${params[3]} string, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} string, ${params[8]} string, ${params[9]} string, ${params[10]} string, ${params[11]} int ) 
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;    
 
     "EducatorEval"*)
         echo "CREATE EXTERNAL TABLE IF NOT EXISTS boston_data.$filename ( ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} float, ${params[4]} float, ${params[5]} float, ${params[6]} float, ${params[7]} float, ${params[8]} float ) 
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;
 
     "Gradsattendingcollege"*)
         echo "CREATE EXTERNAL TABLE IF NOT EXISTS boston_data.$filename ( ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} int, ${params[4]} float, ${params[5]} float, ${params[6]} float, ${params[7]} float, ${params[8]} float, ${params[9]} float, ${params[10]} float, ${params[11]} float ) 
-        COMMENT ‘Location and Administrative info for Boston Public Schools’ 
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;    
 
     "graduates"*)
        echo "CREATE EXTERNAL TABLE IF NOT EXISTS boston_data.$filename ( ${params[0]} string, ${params[1]} int, ${params[2]} int, ${params[3]} float, ${params[4]} float, ${params[5]} float, ${params[6]} float, ${params[7]} float, ${params[8]} float ) 
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;
 
     "Non_Public"*)
         echo "CREATE EXTERNAL TABLE IF NOT EXISTS boston_data.$filename ( ${params[0]} double, ${params[1]} double, ${params[2]} int, ${params[3]} int, ${params[4]} int, ${params[5]} string, ${params[6]} string, ${params[7]} string, ${params[8]} string, ${params[9]} string, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string, ${params[15]} string ) 
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ','
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;    
 
     "Public"*)
         echo "CREATE EXTERNAL TABLE IF NOT EXISTS boston_data.$filename ( ${params[0]} double, ${params[1]} double, ${params[2]} int, ${params[3]} int, ${params[4]} string, ${params[5]} string, ${params[6]} string, ${params[7]} int, ${params[8]} int, ${params[9]} int, ${params[10]} string, ${params[11]} string, ${params[12]} string, ${params[13]} string, ${params[14]} string ) 
         ROW FORMAT DELIMITED 
-        FIELDS TERMINATED BY ‘,’ 
-        LINES TERMINATED BY ‘\n’ 
+        FIELDS TERMINATED BY ',' 
+        LINES TERMINATED BY '\n' 
         STORED AS TEXTFILE;" >> boston-school-data/load-data.hql
         ;;    
 esac
-echo "LOAD DATA INPATH "/data/hive/hive-raw-zone/$filename-stripped.csv" INTO  TABLE boston_data.$filename;" >> boston-school-data/load-data.hql
+echo "LOAD DATA INPATH '/data/hive/hive-raw-zone/$filename-stripped.csv' INTO  TABLE boston_data.$filename;" >> boston-school-data/load-data.hql
 done
 
 #move files into hdfs
