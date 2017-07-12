bddcb254641882c0d0fb81094dec2a4b986fb1a5
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-07-12 14:57:09 -0400

    pythons

--- centos-setup.sh
+++ centos-setup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-yum -y update
-yum -y install yum-utils
-yum -y groupinstall development
-yum -y install https://centos7.iuscommunity.org/ius-release.rpm
-yum -y install python36u
-
-git clone https://github.com/iamemilio/Knowledgent-Intern-Project-2017.git
+if command -v python3 > /dev/null 2>&1; then
+    echo true
+else
+    sudo yum install epel-release
+    sudo yum install python34
+    curl -O https://bootstrap.pypa.io/get-pip.py
+    sudo /usr/bin/python3.4 get-pip.py
+fi
