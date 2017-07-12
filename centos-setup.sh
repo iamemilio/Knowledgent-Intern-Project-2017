00d5f4731c7bda7d3ddd06ccf580ba23868a1d25
Emilio Garcia <Emilio.Garcia@kgi.local>
2017-07-12 13:33:19 -0400

    good to go?

--- centos-setup.sh
+++ centos-setup.sh
@@ -5,3 +5,5 @@ yum -y install yum-utils
 yum -y groupinstall development
 yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 yum -y install python36u
+
+git clone https://github.com/iamemilio/Knowledgent-Intern-Project-2017.git
